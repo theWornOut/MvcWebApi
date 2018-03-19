e639ec2c8a49734b1e775db4d4471c2c4ef461e9
theWornOut <ekremalpayy@gmail.com>
2018-03-20 00:27:04 +0300

    Convention, action based routing

--- MvcWebApi/Controllers/ValuesController.cs
+++ MvcWebApi/Controllers/ValuesController.cs
@@ -7,34 +7,49 @@ using System.Web.Http;
 
 namespace MvcWebApi.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        static List<string> _valueList = new List<string>
         {
-            return new string[] { "value1", "value2" };
+            "value0", "value1", "value2"
+        };
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> ValueGet()
+        {
+            //return new string[] { "value1", "value2" };
+            return _valueList;
         }
 
         // GET api/values/5
-        public string Get(int id)
+        [HttpGet]
+        public string ValueGet(int id)
         {
-            return "value";
+            //return "value";
+            return _valueList[id];
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        [HttpPost]
+        public void ValuePost([FromBody]string value)
         {
+            _valueList.Add(value);
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        [HttpPut]
+        public void ValuePut(int id, [FromBody]string value)
         {
+            _valueList[id] = value;
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        [HttpDelete]
+        public void ValueDelete(int id)
         {
+            _valueList.RemoveAt(id);
         }
     }
 }
